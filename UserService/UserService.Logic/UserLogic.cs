5cff80b26d5b6fa04a2ff4e76a08e519bfc56210
Christopher Phillip Freyburg <cfreyburg@gmail.com>
2018-09-29 17:09:02 -0300

    feat(first commit):first commit

---
+++ UserService/UserService.Logic/UserLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserService.Logic
+{
+    class UserLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
